8d4023ac31c967da621019f7ebfadd69063cc523
dinozoff1981 <tensaeg@yahoo.com>
2024-10-25 11:42:16 +0200

    Add project files.

---
+++ Music-Instrumet-Online-Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Music_Instrumet_Online_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
