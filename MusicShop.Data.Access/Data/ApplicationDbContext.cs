0782b5682f7eb88f5375b487ef7e2ee51d10f3ff
dinozoff1981 <tensaeg@yahoo.com>
2024-10-26 10:28:50 +0200

    category lsit commented

--- MusicShop.Data.Access/Data/ApplicationDbContext.cs
+++ MusicShop.Data.Access/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace MusicShop.Data.Access.Data
         {
 
         }
+
+        //this is category list
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
