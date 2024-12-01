d22c6f86dc178f2eb7b9bb805f74b9ec7edfc65b
dinozoff1981 <tensaeg@yahoo.com>
2024-12-01 05:16:39 +0100

    url to stripe

--- Music-Instrumet-Online-Shop/Areas/Customer/Controllers/CartController.cs
+++ Music-Instrumet-Online-Shop/Areas/Customer/Controllers/CartController.cs
@@ -192,7 +192,7 @@ namespace Music_Instrumet_Online_Shop.Areas.Customer.Controllers
 
             if (applicationUser.CompanyId.GetValueOrDefault() == 0)
             {
-                var domain = Request.Scheme+"://"+Request.Host.Value+"/";
+                var domain = Request.Scheme + "://"  +Request.Host.Value + "/";
 
                 var options = new SessionCreateOptions
                 {
