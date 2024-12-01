d725826310d82acdc65d2fb8ac6e4f695af62296
dinozoff1981 <tensaeg@yahoo.com>
2024-12-01 08:07:00 +0100

    new migration adjested

---
+++ MusicShop.Data.Access/Migrations/20241201070318_adjestmentofnullablevalues.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MusicShop.Data.Access.Migrations
+{
+    /// <inheritdoc />
+    public partial class adjestmentofnullablevalues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- MusicShop.Models/OrderHeader.cs
+++ MusicShop.Models/OrderHeader.cs
@@ -35,20 +35,20 @@ namespace MusicShop.Models
 
 
         [Required]
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
         [Required]
-        public string StreetAddress { get; set; }
+        public string? StreetAddress { get; set; }
         [Required]
-        public string City { get; set; }
+        public string? City { get; set; }
         [Required]
-        public string State { get; set; }
+        public string? State { get; set; }
 
 
 
-        public string PostalCode { get; set; }
+        public string? PostalCode { get; set; }
 
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
